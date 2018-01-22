00e293b8d100cda6ca65099b3547396df9d8cce7
kimhaeyeon <mofari@naver.com>
2018-01-22 15:46:35 +0900

    first

--- app/src/main/java/com/example/user/jbg/MainActivity.java
+++ app/src/main/java/com/example/user/jbg/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this,"테스트입니다",Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,"테스트입니다!!",Toast.LENGTH_SHORT).show();
     }
 }
