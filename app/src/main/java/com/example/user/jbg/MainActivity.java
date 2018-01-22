9e6cabc2c64af88307da86fb30db527ccf00dbbd
kimhaeyeon <mofari@naver.com>
2018-01-22 15:41:43 +0900

    first

--- app/src/main/java/com/example/user/jbg/MainActivity.java
+++ app/src/main/java/com/example/user/jbg/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toast.makeText(MainActivity.this,"테스트",Toast.LENGTH_SHORT).show();
     }
 }
