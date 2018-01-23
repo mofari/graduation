35a30783888be0687d67861f723cd3a73decaaba
parkhyeonbin <gusqls1030@naver.com>
2018-01-23 16:46:22 +0900

    v.0.1.2.3.2

---
+++ app/src/main/java/com/example/user/jbg/BookDeal.java
@@ -0,0 +1,17 @@
+package com.example.user.jbg;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 2018-01-23.
+ */
+
+public class BookDeal extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookdeal);
+
+    }
+}
